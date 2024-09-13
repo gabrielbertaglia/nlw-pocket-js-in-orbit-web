e1cc534c458fb6440bf29342e8c9c397ffd6781e
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-09-13 11:55:36 -0300

    feat: creating goal and form goal

--- src/components/create-goal.tsx
+++ src/components/create-goal.tsx
@@ -1,4 +1,9 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useQueryClient } from '@tanstack/react-query'
 import { X } from 'lucide-react'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { createGoal } from '../http/create-goal'
 import { Button } from './ui/button'
 import {
   DialogClose,
@@ -14,7 +19,46 @@ import {
   RadioGroupItem,
 } from './ui/radio-group'
 
+const createGoalForm = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja realizar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+type CreateGoalForm = z.infer<typeof createGoalForm>
+
+const weekDays = [
+  { value: '1', title: '1x na semana', emoji: '🥱' },
+  { value: '2', title: '2x na semana', emoji: '🙂' },
+  { value: '3', title: '3x na semana', emoji: '😎' },
+  { value: '4', title: '4x na semana', emoji: '😜' },
+  { value: '5', title: '5x na semana', emoji: '🤨' },
+  { value: '6', title: '6x na semana', emoji: '🤯' },
+  { value: '7', title: 'Todos dias da semana', emoji: '🔥' },
+]
+
 export function CreateGoal() {
+  const queryClient = useQueryClient()
+  const {
+    register,
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<CreateGoalForm>({
+    resolver: zodResolver(createGoalForm),
+  })
+
+  async function handleCreateGoal(data: CreateGoalForm) {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    })
+    queryClient.invalidateQueries({ queryKey: ['summary'] }) // atualiza sem precisar do reload
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+
+    reset()
+  }
+
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
@@ -31,7 +75,11 @@ export function CreateGoal() {
             praticando toda semana.
           </DialogDescription>
         </div>
-        <form action="" className="flex-1 flex flex-col justify-between">
+        <form
+          action=""
+          className="flex-1 flex flex-col justify-between"
+          onSubmit={handleSubmit(handleCreateGoal)}
+        >
           <div className="flex flex-col gap-6 ">
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Qual a atividade?</Label>
@@ -39,19 +87,42 @@ export function CreateGoal() {
                 id="title"
                 autoFocus
                 placeholder="Platicar exercícios, meditar, etc..."
+                {...register('title')}
               />
+              {errors.title && (
+                <p className="text-red-400 text-sm">{errors.title.message}</p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Quantas vezes na semana?</Label>
-              <RadioGroup>
-                <RadioGroupItem value="1">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    1x na semana
-                  </span>
-                  <span className="text-lg leading-none">🥱</span>
-                </RadioGroupItem>
-              </RadioGroup>
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={1}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
+                      {weekDays.map(weekDay => (
+                        <RadioGroupItem
+                          value={weekDay.value}
+                          key={weekDay.value}
+                        >
+                          <RadioGroupIndicator />
+                          <span className="text-zinc-300 text-sm font-medium leading-none">
+                            {weekDay.title}
+                          </span>
+                          <span className="text-lg leading-none">
+                            {weekDay.emoji}
+                          </span>
+                        </RadioGroupItem>
+                      ))}
+                    </RadioGroup>
+                  )
+                }}
+              />
             </div>
           </div>
 
